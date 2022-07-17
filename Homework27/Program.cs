48d05ca8428e31de910620601b6634de10be586c
OlegZakharov <varahas@mail.ru>
2022-07-17 18:57:40 +0300

    added homework27

---
+++ Homework27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+namespace Homework27
+{
+    class Programe
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Введите число");
+            string? str = (Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+               sum += int.Parse((str[i]).ToString());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
