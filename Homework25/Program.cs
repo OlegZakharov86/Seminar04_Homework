bcd6270fbf0fe1c46e96e7529976b536c347560e
OlegZakharov <varahas@mail.ru>
2022-07-17 17:57:47 +0300

    added homework25

---
+++ Homework25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+
+namespace Homework25
+{
+    class Programe
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Ведите первое число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите второе число");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            double result = Math.Pow(a,b);
+            Console.WriteLine(result);
+
+        }
+    }
+}
